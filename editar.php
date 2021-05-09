c9f8098fefb85a9c06bf6bc910154a029de19543
Glaubersouza1984 <glauberwzs@hotmail.com>
2021-05-09 15:45:23 -0300

    envio final Glauber

--- editar.php
+++ editar.php
@@ -1,6 +1,21 @@
 <?php
-    include_once 'includes/header.php';
-    include_once 'includes/message.php';
+include_once 'php_action/db_connect.php';
+include_once 'includes/header.php';
+
+//sessão
+session_start();
+
+//verificação
+if(!isset($_SESSION['logado'])):
+    header('Location: login.php');
+endif;
+
+if(isset($_GET['idUsuario'])):
+    $idUsuario = mysqli_escape_string($connect, $_GET['idUsuario']);
+    $sql = "SELECT * FROM usuario WHERE idUsuario = $idUsuario";
+    $resultado = mysqli_query($connect, $sql);
+    $dados = mysqli_fetch_array($resultado);
+endif;
 ?>
 
 <div class="parallax-container">
@@ -8,14 +23,17 @@
 </div>
 <div class="section card-panel teal brown darken-4">
     <div class="row container">
-        <h2 class="header">Cadastro de Usuário Vip</h2>
+        <h2 class="header">Editar Cadastro de Usuário Vip</h2>
         <div class="row">
             <div class="col s12 m8 push-m2">
 
-                <form action="php_action/create.php" method="POST">
+                <form action="php_action/update.php" method="POST">
+
+                    <input type="hidden" name="idUsuario" value="<?php echo $dados['idUsuario'];?>">
 
                     <div class="input-field col s12">
-                        <input type="text" name="nome_usuario" id="nome_usuario" required>
+                        <input type="text" name="nome_usuario" id="nome_usuario"
+                            value="<?php echo $dados['nome_usuario'];?>">
                         <label for="nome_usuario">Nome Completo</label>
                     </div>
 
@@ -25,27 +43,28 @@
                     </div>
 
                     <div class="input-field col s12">
-                        <input type="text" name="login" id="login" required>
+                        <input type="text" name="login" id="login" value="<?php echo $dados['login'];?>">
                         <label for="login">Defina seu Login de Usuário</label>
                     </div>
 
                     <div class="input-field col s12">
-                        <input type="email" name="email" id="email" required>
+                        <input type="email" name="email" id="email" value="<?php echo $dados['email'];?>">
                         <label for="email">Digite seu E-mail</label>
                     </div>
 
                     <div class="input-field col s12">
-                        <input type="number" name="cpf" id="cpf" required>
+                        <input type="text" name="cpf" id="cpf" value="<?php echo $dados['cpf'];?>">
                         <label for="cpf">Digite Seu CPF</label>
                     </div>
 
                     <div class="input-field col s12">
-                        <input type="tel" name="telefone" id="telefone" placeholder="03535518262" required>
+                        <input type="tel" name="telefone" id="telefone" value="<?php echo $dados['telefone'];?>">
                         <label for="telefone">Digite Seu Telefone para Contato com DDD</label>
                     </div>
 
-                    <button type="submit" name="btn-cadastrar" class="btn grey"> Cadastrar </button>
-                    <a href="login.php" type="submit" class="btn black"> Login </a>
+                    <button type="submit" name="btn-editar" class="btn grey"> Atualizar </button>
+                    <a href="login.php" type="submit" class="btn black"> Novo Login </a>
+                    
                 </form>
             </div>
         </div>
@@ -54,7 +73,6 @@
 <div class="parallax-container">
     <div class="parallax"><img src="imagens/ambiente.jpg"></div>
 </div>
-
 
 <?php
     include_once 'includes/footer.php'
--- historico.php
+++ historico.php
@@ -1,7 +1,3 @@
-<html>
-<link rel="stylesheet" type="text/css" href="includes/meuestilo.css">
-</html>
-
 <?php
 
 //Connection
@@ -10,86 +6,122 @@ include_once 'php_action/db_connect.php';
 //Header
 include_once 'includes/header.php';
 
-//Mensagem
-//include_once 'includes/mensagem.php';
+//sessão
+session_start();
 
+//verificação
+if(!isset($_SESSION['logado'])):
+    header('Location: login.php');
+endif;
 
 ?>
 
-<div class="row">
-    <div class="col s12 m6 push-m3">
-    <h3 class="ligth"> Hitorico do Cliente </h3>
-        <table class="striped"> 
-            <thead>
-                <tr>
-                    <th id="table_title">ID da Solicitação</th>
-                    <th id="table_title">Data Solicitação</th>
-                    <th id="table_title">Data do Serviço</th>
-                    <th id="table_title">Serviço</th>
-                    <th id="table_title">Status</th>
-                  
-                </tr>
-            </thead>
+<html>
 
-            <tbody>
-                <?php
+<div class="parallax-container">
+    <div class="parallax"><img src="imagens/navalha-tesoura.jpg"></div>
+</div>
 
-                 //   $idUsuario = mysqli_escape_string($connect, $_POST['idUsuario']);
+<div class="section card-panel teal brown darken-4">
 
+    <div class="row container">
+
+
+        <div class="row">
+            <div class="col s12 m8 push-m2">
+
+                <div style="text-align:center;">
+
+                    <div class="row">
+                        <div class="col s12 m14 push-m0.1">
+                            <h3 class="ligth"> Hitórico do Cliente </h3>
+                            <table class="striped">
+                                <thead>
+                                    <tr>
+                                        <th id="table_title">ID da Solicitação</th>
+                                        <th id="table_title">Data Solicitação</th>
+                                        <th id="table_title">Data do Serviço</th>
+                                        <th id="table_title">Serviço</th>
+                                        <th id="table_title">Status</th>
+
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <?php
+
+                    if(isset($_GET['idUsuario'])):
+                    $idUsuario = mysqli_escape_string($connect, $_GET['idUsuario']);
                     $sql = "SELECT * FROM historico_servicos LEFT JOIN agendar_servicos ON agendar_servicos.idagendar_servicos=historico_servicos.idagendar_servicos 
-                    LEFT JOIN usuario ON usuario.idUsuario=agendar_servicos.usuario WHERE usuario.idUsuario=3";
-
+                    LEFT JOIN usuario ON usuario.idUsuario=agendar_servicos.usuario WHERE usuario.idUsuario= $idUsuario";
                     $resultado = mysqli_query($connect, $sql);
-                    if(mysqli_num_rows($resultado) > 0):
+                    endif;
 
-                    while($dados = mysqli_fetch_array($resultado)):
-                 ?>
-            <tr>
-                    <td id="table_dados"> <?php echo $dados['idagendar_servicos']; ?> </td>
-                    <td id="table_dados"> <?php echo date("d/m/Y", strtotime($dados['data_solicitacao'])); ?> </td>
-                    <td id="table_dados"> <?php echo date("d/m/Y", strtotime($dados['data_servico'])); ?> </td>
-                    <td id="table_dados"> <?php echo $dados['servicos']; ?> </td>
-                    <td id="table_dados"> <?php echo $dados['status']; ?> </td>
-                    
-                   
-                     
-                    </div>
+                if(mysqli_num_rows($resultado) > 0):
 
-                </tr>
+                while($dados = mysqli_fetch_array($resultado)):
+                ?>
+                                    <tr>
+                                        <td id="table_dados"> <?php echo $dados['idagendar_servicos']; ?> </td>
+                                        <td id="table_dados">
+                                            <?php echo date("d/m/Y", strtotime($dados['data_solicitacao'])); ?> </td>
+                                        <td id="table_dados">
+                                            <?php echo date("d/m/Y", strtotime($dados['data_servico'])); ?> </td>
+                                        <td id="table_dados"> <?php echo $dados['servicos']; ?> </td>
+                                        <td id="table_dados"> <?php echo $dados['status']; ?> </td>
+                        </div>
 
-                <?php 
+                        </tr>
+
+                        <?php 
                 endwhile; 
                  else: ?>
 
-                    <tr>
-                    <td id="table_dados">-</td>
-                    <td id="table_dados">-</td>
-                    <td id="table_dados">-</td>
-                    <td id="table_dados">-</td>
-                    <td id="table_dados">-</td>
-                    <td id="table_dados">-</td>
-                    </tr>
+                        <tr>
+                            <td id="table_dados">-</td>
+                            <td id="table_dados">-</td>
+                            <td id="table_dados">-</td>
+                            <td id="table_dados">-</td>
+                            <td id="table_dados">-</td>
+                            <td id="table_dados">-</td>
+                        </tr>
 
-                    
 
-                <?php
+
+                        <?php
                 endif;
                 ?>
 
-            </tbody>
-        </table>
-        <br>
+                        </tbody>
+                        </table>
+                        <br>
 
-             Legenda
-        <br>
-            Serviços:   1-Corte   2-Barba    3-Sobrancelha 
-        <br>
-             Status: 0-Pendente 1-Finalizado 
+                        Legenda
+                        <br>
+                        Serviços: 1-Corte 2-Barba 3-Corte e Barba
+                        <br>
+                        Status: 0-Pendente 1-Finalizado
 
+                        <br>
+                        <br>
+                        <button type="submit" name="btn-sair" class="btn black"><a class="white-text"
+                                href="home.php">Voltar</a></button>
+                        <br />
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+</div>
 
+<div class="parallax-container">
+    <div class="parallax"><img src="imagens/barber-fer2.jpg"></div>
+</div>
+</body>
 
+</html>
 <?php
 //Footer
 include_once 'includes/footer.php';
